c5dd8912774cecca05aceef0e8423c46053835e4
Mohammed Hunain <hunainhsjwjj@gmail.com>
2026-05-03 15:33:44 +0500

    Create FileSorting.cpp

---
+++ FileSorting.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main() {
+    vector <int> arr(5);
+    ofstream fout;
+    
+    cout <<  "Enter input: ";
+    for(int i = 0; i < 5; i++) cin >> arr[i];
+
+    fout.open("List.txt");
+    
+    fout << "Orginal Data: \n";
+    for(int i = 0; i < 5; i++) fout << arr[i] << " ";
+
+    sort(arr.begin(), arr.end());
+
+    fout << "\nSorted Data\n";
+
+    for(int i = 0; i < 5; i++) fout << arr[i] << " ";
+
+    fout.close();
+
+    return 0;
+}
