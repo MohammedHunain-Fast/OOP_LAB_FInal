2f9718d09f38531c6011191662e3d5a81a772988
Mohammed Hunain <hunainhsjwjj@gmail.com>
2026-05-04 15:22:09 +0500

    Add FileReader class for reading and counting uppercase letters
    Implement FileReader class to count uppercase letters in a file.

---
+++ FileReaderSecret.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cctype>
+using namespace std;
+
+class FileReader {
+    private:
+        char ch;
+        int uppercase;
+        public:
+        FileReader(string filename) : uppercase(0) {
+            ifstream fin(filename);
+            if(!fin) throw "File failed to open!\n";
+            while(fin.get(ch)) {
+                if(isupper(ch)) uppercase++;
+            }
+            cout << "File read successfully!\n";
+            fin.close();
+            cout << "File closed!\n";
+        }
+
+        int getUpperCase() const{
+            return uppercase;
+        }
+};
+
+int main() {
+    try {
+    FileReader f1("secret.txt");
+    cout << "Signal: " << f1.getUpperCase();
+    }
+    catch(const char* e)  {
+        cout << "Exception occured: " << e <<  endl;
+    }
+    return 0;
+}
